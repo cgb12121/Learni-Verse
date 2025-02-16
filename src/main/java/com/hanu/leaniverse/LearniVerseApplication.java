b96459a80316da32f61c5412429296eb606e4a54
Cao Gia Bao <148226446+cgb12121@users.noreply.github.com>
2025-02-16 21:02:07 +0700

    Add files via upload

---
+++ src/main/java/com/hanu/leaniverse/LearniVerseApplication.java
@@ -0,0 +1,13 @@
+package com.hanu.leaniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearniVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearniVerseApplication.class, args);
+    }
+
+}
